a2d8d5c67a12fded15617bb2c75843489c49f09a
elkarni bachar <diamobachar06@gmail.com>
2024-11-07 10:11:20 +0100

    initial service-discovery commit

---
+++ src/main/java/com/example/servicediscovery/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.servicediscovery.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${app.security.user.name}")
+    private String username;
+    @Value("${app.security.user.password}")
+    private String password;
+
+    @Bean
+    public SecurityFilterChain filterChainConfiguration(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").hasRole("ADMIN")    // Require authentication for actuator endpoints
+                        .requestMatchers("/eureka/**").permitAll()  // Allow Eureka dashboard access
+                        .anyRequest().authenticated()  // Allow all other requests
+                )
+                .httpBasic(Customizer.withDefaults())  // Enable Basic Authentication (optional)
+                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF for simplicity; adjust as needed
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+
+        UserDetails admin = User.builder()
+                .username(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();  // Password encoder
+    }
+}
